b36e739a55cf319cd3796a30486a63e3b9bf6f5b
juber <juberk1010@gmail.com>
2023-12-06 11:05:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const topButton = document.querySelector('.go-to-top');
+window.addEventListener('scroll' , () => {
+    console.log(document.documentElement)
+    // console.log(scrollX)
+    // console.log(window.innerHeight)
+    // console.log(document.body.offsetHeight)
+    // console.log(document.body.offsetWidth)
+    // if(window.scrollY + window.innerHeight > document.body.offsetHeight){
+    //     // topButton.style.opactiy = 1;
+    //     // document.querySelector('.go-to-top').s
+    //     console.log(topButton)
+    //     topButton.classList.add('active');
+    // }else{
+        
+    //     topButton.classList.remove('active');
+    // }
+})
